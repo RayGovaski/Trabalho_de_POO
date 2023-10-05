e152311f5968721e1191f26cc29b5f607f09c2a2
Maria Eduarda <127451410+dudatt@users.noreply.github.com>
2023-10-05 19:14:48 -0300

    Update Funcionarios.java

--- .idea/Funcionarios.java
+++ .idea/Funcionarios.java
@@ -1,9 +1,9 @@
+class Funcionario {
     private double salario;
     private int cpf;
-
+    
     Funcionarios(double salario, int cpf){
         this.salario = salario;
         this.cpf = cpf;
-    }
-
+        }
 }
