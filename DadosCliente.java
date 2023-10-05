556edd6afecaa612197cadfcdc44013648fa9720
ImnotSoul <116972597+ImnotSoul@users.noreply.github.com>
2023-10-05 20:40:59 -0300

    DadosClientes completo

--- DadosCliente.java
+++ DadosCliente.java
@@ -6,6 +6,30 @@ class DadosCliente{
     DadosCliente(int cpf, String nome, String telefone){
         this.cpf = cpf;
         this.nome = nome;
+        this.telefone = telefone;//
+    }
+
+    public int getCpf(){
+        return cpf
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+    public String toString() {
+        return "CPF: " + cpf + "\nNome: " + nome + "\nTelefone: " + telefone;
     }
 }
