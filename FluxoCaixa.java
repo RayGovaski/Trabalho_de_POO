d41332c66098ad27e275671ea080842ea8260089
RayGovaski <rayygovaski@gmail.com>
2023-10-05 20:07:55 -0300

    Update FluxoCaixa.java

--- FluxoCaixa.java
+++ FluxoCaixa.java
@@ -4,4 +4,21 @@ class FluxoCaixa{
     FluxoCaixa(double saldo){
         this.saldo = saldo;
     }
+
+    public void adicionarSaldo(double dinheiro){
+    saldo += dinheiro;
+    System.out.println("Saldo atual é: " + saldo);    
+    }
+
+    public void retirarSaldo(double valor){
+        if (dinheiro<= saldo){
+            saldo -= dinheiro;
+            System.out.println("Saldo atual: " + saldo)
+        }
+        else{
+            System.out.println("Saldo insuficiente. A operação não realizada")
+        }
+        public double getsaldo(){
+            return saldo;
+        }
 }
