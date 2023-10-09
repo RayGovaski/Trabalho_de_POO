cc2d42ba2d9a9ee8bf8e5f33eecbe199bb7b9b3f
Maria Eduarda <dudamelobarbosa@gmail.com>
2023-10-09 15:26:51 -0300

    Update ControleEstoque.java

--- ControleEstoque.java
+++ ControleEstoque.java
@@ -7,18 +7,28 @@ class ControleEstoque{
     ControleEstoque(String itens){
         this.itensEstoque = itens;
     }
+
+    public String getItens() {
+        return itensEstoque;
+    }
+
+    public void criarLista(String itens) {
+        
+    }
+
+    public void setItens(String itens) {
+  
+    }
 } 
 
  class Main {
-    public static void main(String[] args) {
-        ArrayList<ControleEstoque> listaItens = new ArrayList<>();
+    public static void main(String[] args) {  //agora tenho que trocar o tipo String p classe p conseguir usar o atributo como lista
+        ArrayList<String> listaItens = new ArrayList<String>();
+        listaItens.add("Café");
+        listaItens.add("açúcar");
 
-        ControleEstoque i1 = new ControleEstoque("café");
-
-        listaItens.add(i1);
-
-        for (int i = 0; i < listaItens.size(); i++) {
-            System.out.println(listaItens.get(i));
-        }   
+        for (int i = 0; i < listaItens.size(); i ++) {
+        System.out.println(listaItens.get(i));
     }
+    }    
 }
